f4aff5e772058875abbd294fd3cfafdacbef2de8
Coby Durling <Coby Durling>
2017-04-27 20:28:41 -0400

    Basic clock functionality

--- clock/src/App.js
+++ clock/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './NavBar';
+import Clock from './Clock';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar title={"Clock"}/>
+        <Clock/>
       </div>
     );
   }
--- clock/src/Clock.js
+++ clock/src/Clock.js
@@ -1,3 +1,34 @@
 import React, { Component } from 'react';
 
+class Clock extends Component {
+    constructor(props) {
+        super(props);
+        this.calculateTime = this.calculateTime.bind(this);
+        this.state = {
+            time: null
+        };
+    }
+
+    calculateTime() {
+        const dateTime = new Date();
+        const h = String(dateTime.getHours());
+        const m = String(dateTime.getMinutes());
+        this.setState({
+            time: h.concat(":", m)
+        });
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(this.calculateTime, 1000);
+    }
+
+    render() {
+        return (
+          <div className="Clock">
+            {this.state.time}
+          </div>
+        );
+    }
+}
+
 export default Clock
